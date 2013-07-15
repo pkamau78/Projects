28796e339440bb0055c87c5e9aeba3c0e5952f28
Huu Nguyen <whoshuu@gmail.com>
2013-07-14 20:22:36 -0700

    Input validation for pi.py

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
+import math
 
 
 def main():
-    pass
+    while True:
+        s = raw_input("How many digits of pi do you want to see? ")
+        try:
+            digits = int(s)
+            if digits > 0:
+                break
+            else:
+                print "Enter a proper nonnegative integer."
+        except ValueError:
+            print "Enter a proper nonnegative integer."
+    print digits
 
 
 if __name__ == "__main__":
